9d7c285ab1c95c3c8e9fd5ad24451c585f88211b
joebarker <joe.joeb29@gmail.com>
2024-11-20 22:11:12 +0700

    Pokemon local converter should convert PokemonLocalModel and handle edge cases

---
+++ local/test/converters/pokemon_local_converter_should.dart
@@ -0,0 +1,94 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:local/converters/pokemon_local_converter.dart';
+import 'package:repository/models/local/pokemon_local_model.dart';
+import 'package:local/database_constants.dart';
+
+void main() {
+  late PokemonLocalConverter converter;
+  const int pokemonId = 1;
+  const String pokemonName = "Sample Pokemon";
+  const List<String> pokemonTypes = ["Grass", "Poison"];
+  const String pokemonTypesAsString = "Grass,Poison";
+  const String pokemonFrontSpriteUrl = "https://example.com/example.png";
+  const Map<String, int> pokedexEntryNumbers = {'original-johto': 5};
+
+  setUp(() {
+    converter = PokemonLocalConverter();
+  });
+
+  test('converts valid PokemonLocalModel', () {
+    final pokemon = PokemonLocalModel(
+      id: pokemonId,
+      name: pokemonName,
+      types: pokemonTypes,
+      frontSpriteUrl: pokemonFrontSpriteUrl,
+      pokedexEntryNumbers: pokedexEntryNumbers,
+    );
+
+    final result = converter.convert(pokemon);
+
+    expect(result, {
+      DatabaseColumnNames.id: pokemonId,
+      DatabaseColumnNames.name: pokemonName,
+      DatabaseColumnNames.types: pokemonTypesAsString,
+      DatabaseColumnNames.frontSpriteUrl: pokemonFrontSpriteUrl,
+    });
+  });
+
+  test('converts a PokemonLocalModel with null types', () {
+    final pokemon = PokemonLocalModel(
+      id: pokemonId,
+      name: pokemonName,
+      types: null,
+      frontSpriteUrl: pokemonFrontSpriteUrl,
+      pokedexEntryNumbers: pokedexEntryNumbers,
+    );
+
+    final result = converter.convert(pokemon);
+
+    expect(result, {
+      DatabaseColumnNames.id: pokemonId,
+      DatabaseColumnNames.name: pokemonName,
+      DatabaseColumnNames.types: null,
+      DatabaseColumnNames.frontSpriteUrl: pokemonFrontSpriteUrl,
+    });
+  });
+
+  test('converts a PokemonLocalModel with empty types', () {
+    final pokemon = PokemonLocalModel(
+      id: pokemonId,
+      name: pokemonName,
+      types: [],
+      frontSpriteUrl: pokemonFrontSpriteUrl,
+      pokedexEntryNumbers: pokedexEntryNumbers,
+    );
+
+    final result = converter.convert(pokemon);
+
+    expect(result, {
+      DatabaseColumnNames.id: pokemonId,
+      DatabaseColumnNames.name: pokemonName,
+      DatabaseColumnNames.types: '',
+      DatabaseColumnNames.frontSpriteUrl: pokemonFrontSpriteUrl,
+    });
+  });
+
+  test('converts a PokemonLocalModel with null frontSpriteUrl', () {
+    final pokemon = PokemonLocalModel(
+      id: pokemonId,
+      name: pokemonName,
+      types: pokemonTypes,
+      frontSpriteUrl: null,
+      pokedexEntryNumbers: pokedexEntryNumbers,
+    );
+
+    final result = converter.convert(pokemon);
+
+    expect(result, {
+      DatabaseColumnNames.id: pokemonId,
+      DatabaseColumnNames.name: pokemonName,
+      DatabaseColumnNames.types: pokemonTypesAsString,
+      DatabaseColumnNames.frontSpriteUrl: null,
+    });
+  });
+}
