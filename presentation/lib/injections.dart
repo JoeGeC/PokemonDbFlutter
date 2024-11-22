6e910cd2fdd88afb3ac61e31feb797d88a18ac24
joebarker <joe.joeb29@gmail.com>
2024-11-22 18:59:56 +0700

    Fixed pokemon fallback image

--- presentation/lib/injections.dart
+++ presentation/lib/injections.dart
@@ -1,5 +1,4 @@
 import 'package:get_it/get_it.dart';
-import 'package:presentation/pokedex/bloc/pokedex_pokemon/pokedex_pokemon_bloc.dart';
 import 'package:presentation/pokedex/converters/pokedex_presentation_converter.dart';
 import 'package:presentation/pokedex/converters/pokedex_presentation_converter_impl.dart';
 import 'package:presentation/pokemon/converters/pokemon_presentation_converter.dart';
@@ -10,6 +9,4 @@ final getIt = GetIt.instance;
 setupPresentationInjections(){
   getIt.registerSingleton<PokedexPokemonPresentationConverter>(PokedexPokemonPresentationConverterImpl());
   getIt.registerSingleton<PokedexPresentationConverter>(PokedexPresentationConverterImpl(getIt()));
-
-  getIt.registerSingleton<PokedexPokemonBloc>(PokedexPokemonBloc(getIt(), getIt()));
 }
